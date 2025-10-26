0486f9bd389c3d5ca12a76cb7b3ec2f646887b02
Avidasevi <andersonribas1357@gmail.com>
2025-10-26 01:59:39 -0300

    build(ts): configurar scripts build/start e Node 20.x

--- client/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://pshmizivmvzjvwxouygj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaG1peml2bXZ6anZ3eG91eWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MDE5NTUsImV4cCI6MjA3MzE3Nzk1NX0.kToicwD1vKUTe9mPE1JFbV_PdtRmMJ5PpXYFJvMN3GM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
